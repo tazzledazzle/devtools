3c2e6aec93a7498a84bd82774ad8dba598912206
Terence Schumacher <terenceschumacher@gmail.com>
2026-03-11 12:43:49 -0700

    F: Ingestion baseline (INGBASE)

---
+++ src/sources/event_sources.py
@@ -0,0 +1,7 @@
+# Implementing EventSources + EventSourcesApps, EventSources ABC with events() -> Iterator[dict], implement EventSourcesApps using src/generators/sythetic_payments.py
+
+class EventSource:
+    """ events() -> Iterator[dict]"""
+
+
+class EventSourcesApps:
